4b0e32ded99de680f128978e3b5170e710c14d02
Tati <tati.moon89@gmail.com>
2024-10-20 20:04:16 +0200

    update

--- task2/variables.tf
+++ task2/variables.tf
@@ -71,7 +71,7 @@ variable "availability_zones" {
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for the bastion host."
   type        = string
-  default     = "new-key-pair1"
+  default     = "my-new-key1"
 }
 
 variable "allowed_ip_cidr" {
