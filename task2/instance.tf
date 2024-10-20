88372220c0f1e6f98b0384296ec2ef3e28277d8f
Tati <tati.moon89@gmail.com>
2024-10-20 15:57:31 +0200

    edit 2

--- task2/instance.tf
+++ task2/instance.tf
@@ -15,6 +15,26 @@ resource "aws_instance" "public_instances" {
   }
 }
 
+resource "aws_instance" "private_instances" {
+  count = length(var.private_subnet_cidrs)
+
+  ami           = var.ec2_ami_amazon_linux
+  instance_type = "t2.micro"
+  key_name      = var.ssh_key_name
+
+  subnet_id                   =  element(aws_subnet.private_subnet[*].id, count.index)
+  vpc_security_group_ids = [
+    aws_security_group.allow_ssh.id,
+    aws_security_group.allow_icmp.id
+  ]
+  associate_public_ip_address = true
+
+   tags = {
+    Name = "Private Instance  ${count.index + 1}"
+    Environment = "Development"
+  }
+}
+
 resource "aws_instance" "bastion_host" {
   ami           = var.ec2_ami_amazon_linux
   instance_type = "t2.micro"
