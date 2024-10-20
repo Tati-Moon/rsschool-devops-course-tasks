ac6071ee3ddaf6f768604087965830aa298c90c9
Tati <tati.moon89@gmail.com>
2024-10-20 14:34:05 +0200

    update2

--- task2/route_table.tf
+++ task2/route_table.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "private_route_table" {
 
 resource "aws_route_table_association" "private_subnet_association" {
   count          = length(var.private_subnet_cidrs)
-  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  subnet_id      = element(aws_subnet.private_subnet[*].id, count.index)
   route_table_id = aws_route_table.private_route_table.id
 }
 
@@ -32,6 +32,6 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table_association" "public_subnet_association" {
   count          = length(var.public_subnet_cidrs)
-  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  subnet_id      = element(aws_subnet.public_subnet[*].id, count.index)
   route_table_id = aws_route_table.public_route_table.id
 }
