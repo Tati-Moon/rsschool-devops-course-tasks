d71014fb9ca197ad35b09e901352dd6793398eeb
Tati <tati.moon89@gmail.com>
2024-11-10 18:34:35 +0100

    Task 4: Jenkins Installation and Configuration

--- task2/security_group.tf
+++ task2/security_group.tf
@@ -1,119 +1,3 @@
-resource "aws_security_group" "bastion" {
-  name        = "bastion-sg"
-  description = "Security group for bastion host"
-  vpc_id      = aws_vpc.my_project_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
- tags = {
-      Name = "bastion-instance"
-    }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  vpc_id      = aws_vpc.my_project_vpc.id
-  name        = "allow_ssh"
-  description = "Security group allowing SSH traffic"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "all"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "Allow SSH"
-  }
-}
-
-resource "aws_security_group" "allow_icmp" {
-  vpc_id      = aws_vpc.my_project_vpc.id
-  name        = "allow_icmp"
-  description = "Security group allowing ICMP (ping) traffic"
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "Allow ICMP"
-  }
-}
-
-resource "aws_security_group" "private_instance" {
-  name        = "private-instance-sg"
-  description = "Security group for private instance"
-  vpc_id      = aws_vpc.my_project_vpc.id
-
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
-    description     = "Allow SSH from bastion-host."
-  }
-
-  ingress {
-    from_port       = 8
-    to_port         = 0
-    protocol        = "icmp"
-    security_groups = [aws_security_group.bastion.id, aws_security_group.public_instance.id]
-    description     = "Allow ICMP echo from bastion-host and public instances."
-  }
-
-  ingress {
-    from_port       = 0
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.public_instance.id]
-    description     = "Allow all TCP traffic from public instances"
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self        = true
-    description = "Allow all traffic between instances with this security group."
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Anywhere."
-  }
-
- tags = {
-      Name = "private-instance"
-    }
-}
-
 resource "aws_security_group" "public_instance" {
   name        = "public-instance-sg"
   description = "Security group for public instances"
@@ -124,7 +8,6 @@ resource "aws_security_group" "public_instance" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow HTTP from anywhere"
   }
 
   ingress {
@@ -132,23 +15,21 @@ resource "aws_security_group" "public_instance" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow HTTPS from anywhere"
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow SSH from your IP"
+    cidr_blocks = ["0.0.0.0/0"]# SSH 
   }
 
   ingress {
-    from_port   = 8
-    to_port     = 0
+    description = "icmp ping and other messages are allowed"
+    from_port   = -1
+    to_port     = -1
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow ICMP echo from anywhere"
   }
 
   egress {
@@ -156,50 +37,164 @@ resource "aws_security_group" "public_instance" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
   }
 
   tags = {
-      Name = "public-instance"
-    }
+      Name = "PublicInstanceSecurityGroup"
+  }
 }
 
-resource "aws_security_group" "k3s" {
+
+resource "aws_security_group" "bastion" {
+  name        = "bastion-sg"
+  description = "Security group for bastion host"
   vpc_id      = aws_vpc.my_project_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # SSH доступ ко всем
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "BastionHostSecurityGroup"
+  }
+}
+
+resource "aws_security_group" "private_instance" {
+  name        = "private-instance-sg"
+  description = "Security group for private instance"
+  vpc_id      = aws_vpc.my_project_vpc.id
+
+  # SSH доступ только с Bastion Host
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+    description     = "Allow SSH from bastion host"
+  }
+
+  # Разрешение всех соединений внутри группы
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+    description = "Allow all traffic within the group"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+      Name = "PrivateInstanceSecurityGroup"
+  }
+}
+
+# Security Group для K3s и Jenkins
+resource "aws_security_group" "k3s" {
+  name        = "k3s-security-group"
+  vpc_id      = aws_vpc.my_project_vpc.id
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]# SSH 
+  }
+
+  ingress {
+    description = "icmp ping and other messages are allowed"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Kubernetes API"
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = ["203.0.113.0/24"]
+    cidr_blocks = ["0.0.0.0/0"] # доступ к API k8s
   }
 
   ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Jenkins доступ
+  }
+
+  ingress {
+    description = "Public"
+    from_port   = 32000
+    to_port     = 32000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
-    Name = "k3s-security-group"
+    Name = "K3sJenkinsSecurityGroup"
   }
 }
 
+# Ingress для Grafana
 resource "aws_security_group_rule" "grafana_ingress" {
-  type        = "ingress"
-  from_port   = 3000
-  to_port     = 3000
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Grafana доступ ко всем
   security_group_id = aws_security_group.k3s.id
 }
